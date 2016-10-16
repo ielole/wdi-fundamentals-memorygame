eb724dbc618f5c7d364a64e452df6b2e51620cc4
ielole <ieloletin@gmail.com>
2016-10-16 17:21:33 -0400

    added if else logic to main.js

--- starter-code/CardGame/main.js
+++ starter-code/CardGame/main.js
@@ -3,3 +3,28 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+if (cardOne === cardThree) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+if (cardTwo === cardThree) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+if (cardOne === cardFour) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
