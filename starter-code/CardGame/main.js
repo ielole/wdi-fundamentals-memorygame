b204dd6002ba0b25897c6c0cbe54b1e4661adcff
ielole <ieloletin@gmail.com>
2016-10-17 07:29:46 -0400

    finished up game, still needs work

--- starter-code/CardGame/main.js
+++ starter-code/CardGame/main.js
@@ -1,8 +1,5 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+
 
 /*if (cardTwo === cardFour) {
 	alert("You found a match!");
@@ -28,11 +25,47 @@ if (cardOne === cardFour) {
 else {
 	alert("Sorry, try again.");
 }*/
+
+var cards = ["queen", "queen","king", "king"];
+
+var cardsInPlay = [];
+
 var board = getElementId('game-board');
-var CreateCards = function() {
- for( var i = 0; i<5; i++);
-var cardDeck = document.createElement('div');
-game-board.appendChild(cardDeck);
-cardDeck.className = 'card';
+
+function createBoard() {}
+	for( var i = 0; i<cards.length; i++); {
+var cardElement = document.createElement('div');
+cardElement.setAttribute('data-card', cards[i]);
+cardElement.addEventListener('click', isTwoCards);
+board.appendChild(cardElement);
+	}
 }
-CreateCards(i);
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  	if (this.getAttribute('data-card') === 'king') {
+  		this.innerHTML = "<img src='http://imgur.com/gallery/yOreVKY'>";
+  	}
+  	else {
+  		this.innerHTML = "<img src='<http://imgur.com/gallery/NvQSc8f'>";
+  	}
+
+  if (cardsInPlay.length === 2) {
+
+    isMatch(cardsInPlay);
+
+    cardsInPlay = [];
+
+  }
+
+}
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+	}
+}
+ createBoard(); 
+
